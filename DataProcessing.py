2b3492cb1e154a9a5e3448ed171a53f8a3a18862
Jeff Haltom <52675783+jahaltom@users.noreply.github.com>
2022-04-07 12:37:54 -0500

    Add files via upload

---
+++ DataProcessing.py
@@ -0,0 +1,75 @@
+import os
+import pandas as pd
+from scipy.stats import chi2_contingency
+
+
+os.chdir(r"C:\Users\15154\Desktop\OFR10_Nidia\scripts")
+
+
+#Make all keywords lowercase and remake files as tsv. 
+with open ("Dead.txt") as f:
+        dead_list=f.read().splitlines()
+        temp=[x.lower() for x in dead_list]
+        dead_list=temp
+        dead_df=pd.DataFrame(dead_list).drop_duplicates()
+        dead_df.to_csv("Dead.tsv",sep='\t',mode='w',index=None,header=None)
+        
+with open ("Mild.txt") as f:
+        mild_list=f.read().splitlines()
+        temp=[x.lower() for x in mild_list]
+        mild_list=temp
+        mild_df=pd.DataFrame(mild_list).drop_duplicates()
+        mild_df.to_csv("Mild.tsv",sep='\t',mode='w',index=None,header=None)
+          
+        
+with open ("Moderate.txt") as f:
+        moderate_list=f.read().splitlines()
+        temp=[x.lower() for x in moderate_list]
+        moderate_list=temp
+        moderate_df=pd.DataFrame(moderate_list).drop_duplicates()
+        moderate_df.to_csv("Moderate.tsv",sep='\t',mode='w',index=None,header=None)
+    
+with open ("Very_Severe.txt") as f:
+        very_severe_list=f.read().splitlines()
+        temp=[x.lower() for x in very_severe_list]
+        very_severe_list=temp
+        very_severe_df=pd.DataFrame(very_severe_list).drop_duplicates()
+        very_severe_df.to_csv("Very_Severe.tsv",sep='\t',mode='w',index=None,header=None)
+
+        
+with open ("VeryMild_Asymptomatic.txt") as f:
+        veryMild_asymptomatic_list=f.read().splitlines()
+        temp=[x.lower() for x in veryMild_asymptomatic_list]
+        veryMild_asymptomatic_list=temp
+        veryMild_asymptomatic_df=pd.DataFrame(veryMild_asymptomatic_list).drop_duplicates()
+        veryMild_asymptomatic_df.to_csv("VeryMild_Asymptomatic.tsv",sep='\t',mode='w',index=None,header=None)
+
+ 
+        
+#Some pateints will be excluded becuase they failed to have a status keyword. Merge pateint dataset with above keywords to only get pateints that have status keywords.
+#Read in pateint dataset
+Data = pd.read_csv("DeltaData.txt",sep='\t' )
+#Make keywords lowercase
+Data['Status'] = Data['Status'].str.lower()
+#Combine all keywords into df.
+merge_list=dead_list+mild_list+moderate_list+very_severe_list+veryMild_asymptomatic_list
+merge_list=pd.DataFrame({'Status': merge_list})
+
+#Merge 
+DeltaData=pd.merge(merge_list,Data,on=['Status'])
+DeltaData.to_csv("DeltaData.tsv",sep='\t',mode='w',index=None)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ ORF10_Chi-Square_Test_2.py
@@ -0,0 +1,195 @@
+import os
+import pandas as pd
+from scipy.stats import chi2_contingency
+
+
+os.chdir(r"C:\Users\15154\Desktop\OFR10_Nidia\scripts")
+
+
+
+
+#Given a dataframe of patients, returns total # patients and total # patients belonging to each condition. Returns list of ints.
+def catagorize(df):
+    #Total # patients
+    Total=len(df.index)
+    dead=0
+    for i in dead_list:
+        dead=dead+len(df[df['Status'] == i])
+    mild=0
+    for i in mild_list:
+        mild=mild+len(df[df['Status'] == i])
+    very_severe=0
+    for i in very_severe_list:
+        very_severe=very_severe+len(df[df['Status'] == i])
+    moderate=0
+    for i in moderate_list:
+        moderate=moderate+len(df[df['Status'] == i])
+    veryMild_asymptomatic=0
+    for i in veryMild_asymptomatic_list:
+        veryMild_asymptomatic=veryMild_asymptomatic+len(df[df['Status'] == i])
+
+    return (Total,veryMild_asymptomatic,mild,moderate,very_severe,dead)
+
+
+
+def Condition(CondState):
+        #Get index number of condition(s)
+        if CondState == "Very-Mild_Asymptomatic":
+            return 1
+        if CondState == "Mild":
+            return 2
+        if CondState == "Moderate":
+            return 3
+        if CondState == "Very_Severe":
+            return 4
+        if CondState == "Dead":
+            return 5
+   
+
+
+#Takes in a dataframe for the whole Strain(df_Strain), and a df for a particular mutation in that Strain (df_mut). Strain and mut are strings that contain Strain name and mutation(E.g. A29558T).
+#CondState and OtherCondState is the condition of the patient(Mild, Very_Severe, Moderate, Very-Mild_Asymptomatic, Dead).
+
+#Performs Chi-Square Test for two specific groups
+def ChiSqr_Two(Strain,mut,df_mut,df_Strain,CondState,OtherCondState,results):
+        mut_list=catagorize(df_mut)
+        Strain_list=catagorize(df_Strain)
+
+        #Get index number of condition(s)
+        CondNum=Condition(CondState) 
+        OtherCondNum=Condition(OtherCondState)
+
+
+
+        mut_total=mut_list[0]
+        #Need at leadt 3 patients with mutation.
+   
+        Strain_total=Strain_list[0]
+        mut_Cond=mut_list[CondNum]
+        clad_Cond=Strain_list[CondNum]
+        #Other Condition
+        mut_OtherCond=mut_list[OtherCondNum]
+        clad_OtherCond=Strain_list[OtherCondNum]
+
+
+        #construct contingency table
+        isCond=["Cond"]*mut_Cond
+        isMut=["Yes"]*mut_Cond
+
+        isCond=isCond+["other_Cond"]*(mut_OtherCond)
+        isMut=isMut+["Yes"]*(mut_OtherCond)
+
+        isCond=isCond+["Cond"]*(clad_Cond-mut_Cond)
+        isMut=isMut+["no"]*(clad_Cond-mut_Cond)
+
+        isCond=isCond+["other_Cond"]*(clad_OtherCond-mut_OtherCond)
+        isMut=isMut+["no"]*(clad_OtherCond-mut_OtherCond)
+
+        df = pd.DataFrame({'isCond' : isCond ,'isMut' : isMut })
+        contigency= pd.crosstab(df['isCond'], df['isMut'])
+
+        
+        try:
+            # Chi-square test of independence.
+            c, p, dof, expected = chi2_contingency(contigency)
+            
+            #Write results
+            #Get the number of patients without mutation for each condition. 
+            nonMut_list=[x - y for x, y in zip(Strain_list, mut_list)]
+            #Get stats
+            mut_list=[Strain+" ("+str(Strain_total)+")",mut+" ("+str(mut_total)+")","Mut+"]+list(mut_list)+[p]         
+            nonMut_list=[Strain+" ("+str(Strain_total)+")",mut+" ("+str(mut_total)+")","Mut-"]+list(nonMut_list)+[p]
+            results_temp=pd.DataFrame([mut_list,nonMut_list],columns=['Strain','Mutation','Has Mutation','Total','Very-Mild_Asymptomatic', 'Mild', 'Moderate', 'Very_Severe', 'Dead',CondState+" vs "+OtherCondState]) 
+            results_temp=results_temp.drop(['Total'], axis=1)
+            results.append(results_temp)
+
+
+        except: pass
+
+
+
+#Performs Chi-Square Test for 1 specific group vs all others.
+def ChiSqr_All(Strain,mut,df_mut,df_Strain,CondState,results):
+        mut_list=catagorize(df_mut)
+        Strain_list=catagorize(df_Strain)
+
+        #Get index number of condition(s)
+        CondNum=Condition(CondState)
+
+
+        mut_total=mut_list[0]
+        #Need at leadt 3 patients with mutation.
+
+        Strain_total=Strain_list[0]
+        mut_Cond=mut_list[CondNum]
+        clad_Cond=Strain_list[CondNum]
+
+
+
+        #construct contingency table
+        isCond=["Cond"]*mut_Cond
+        isMut=["Yes"]*mut_Cond
+
+        isCond=isCond+["not_Cond"]*(mut_total-mut_Cond)
+        isMut=isMut+["Yes"]*(mut_total-mut_Cond)
+
+        isCond=isCond+["Cond"]*(clad_Cond-mut_Cond)
+        isMut=isMut+["no"]*(clad_Cond-mut_Cond)
+
+        isCond=isCond+["not_Cond"]*((Strain_total-mut_total)-(clad_Cond-mut_Cond))
+        isMut=isMut+["no"]*((Strain_total-mut_total)-(clad_Cond-mut_Cond))
+
+        contigency = pd.DataFrame({'isCond' : isCond ,'isMut' : isMut })
+        contigency= pd.crosstab(contigency['isCond'], contigency['isMut'])
+
+        # Chi-square test of independence.
+        c, p, dof, expected = chi2_contingency(contigency)
+
+        #Write results
+ 
+        #Get the number of patients without mutation for each condition. 
+        nonMut_list=[x - y for x, y in zip(Strain_list, mut_list)]
+        #Get stats
+        mut_list=[Strain+" ("+str(Strain_total)+")",mut+" ("+str(mut_total)+")","Mut+"]+list(mut_list)+[p]         
+        nonMut_list=[Strain+" ("+str(Strain_total)+")",mut+" ("+str(mut_total)+")","Mut-"]+list(nonMut_list)+[p]
+        results_temp=pd.DataFrame([mut_list,nonMut_list],columns=['Strain','Mutation','Has Mutation','Total','Very-Mild_Asymptomatic', 'Mild', 'Moderate', 'Very_Severe', 'Dead',CondState+" vs All"]) 
+        results_temp=results_temp.drop(['Total'], axis=1)
+        results.append(results_temp)
+
+
+
+#Keywords that place a patient in a category
+with open ("Dead.tsv") as f:
+        dead_list=f.read().splitlines()
+with open ("Mild.tsv") as f:
+        mild_list=f.read().splitlines()
+with open ("Moderate.tsv") as f:
+        moderate_list=f.read().splitlines()
+with open ("Very_Severe.tsv") as f:
+        very_severe_list=f.read().splitlines()
+with open ("VeryMild_Asymptomatic.tsv") as f:
+        veryMild_asymptomatic_list=f.read().splitlines()
+
+#Read in COVID data
+Data = pd.read_csv("AlphaData.tsv",sep='\t')
+#Create dict of dfs with each Strain as a key. The dfs will contain patients that go with the Strain.
+Strains = dict(tuple(Data.groupby('Strain')))
+results=[]
+for Strain in Strains.keys():
+   #Create dict of dfs with each mutation as a key. The dfs will contain patients that go with the mutation.
+   muts=dict(tuple(Strains[Strain].groupby('substitutions')))
+   
+   for mut in muts.keys():
+       ChiSqr_Two(Strain,mut,muts[mut],Strains[Strain],"Mild","Dead",results)
+       ChiSqr_Two(Strain,mut,muts[mut],Strains[Strain],"Mild","Moderate",results)
+       ChiSqr_Two(Strain,mut,muts[mut],Strains[Strain],"Moderate","Dead",results)
+       ChiSqr_Two(Strain,mut,muts[mut],Strains[Strain],"Very-Mild_Asymptomatic","Dead",results)
+       ChiSqr_Two(Strain,mut,muts[mut],Strains[Strain],"Very_Severe","Mild",results)
+       ChiSqr_Two(Strain,mut,muts[mut],Strains[Strain],"Very_Severe","Dead",results)
+       ChiSqr_Two(Strain,mut,muts[mut],Strains[Strain],"Very_Severe","Very-Mild_Asymptomatic",results)
+       ChiSqr_All(Strain,mut,muts[mut],Strains[Strain],"Dead",results)
+
+results = pd.concat(results)
+results.to_csv("Alpha.tsv",sep='\t',mode='w',index=None)
+
+ 
