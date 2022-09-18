39cbbc9408ee03267ef51c53837ca488d4ef0cc4
Jeff Haltom <52675783+jahaltom@users.noreply.github.com>
2022-09-18 18:33:57 -0500

    Update Muts2fasta.py

--- Muts2fasta.py
+++ Muts2fasta.py
@@ -1,11 +1,16 @@
+import pandas as pd
+
+
+
+
 
 
 #Wildtype Sars seq
 wtNSeq = open("data/ORF10_NC_045512.2_29557-29671.txt", "r")
 wtNSeq=wtNSeq.read().splitlines()[0]
 #List of substitution mutations
-subs = open("data/subs.txt", "r")
-subs=subs.read().splitlines()
+subs=pd.read_csv("DeltaDataWithKeywords.tsv",sep="\t")["substitutions"].dropna().drop_duplicates().tolist()
+
 
 
 #Make fasta file of ORF10 seqs with mutations. wt ORF10 on top. 
@@ -15,13 +20,31 @@ fasta.write('\n')
 fasta.write(wtNSeq[1:len(wtNSeq)])
 fasta.write('\n')
 for sub in subs:
-        wt=sub[0]
-        mut=sub[len(sub)-1]
-        pos=int(sub[1:len(sub)-1])-29557 #29557 is ORF10 start position in sarsCov2 genome. 
-        #Check that wt allele in sub is what it should be according to wtNSeq. Write new nt seq with mutation to fasta.
-        if wtNSeq[pos] == wt:
+        if "," in sub: #Multiple substitution mutations in at least 1 viral seq. 
+            MutNSeq=wtNSeq #To add multiple mutations to
+            #List mutations
+            mult_mut=sub.split(',')
+            for m in mult_mut: 
+                wt=m[0]
+                mut=m[len(m)-1]
+                pos=int(m[1:len(m)-1])-29557 #29557 is ORF10 start position in sarsCov2 genome. 
+                #Check that wt allele in sub is what it should be according to wtNSeq. Write new nt seq with mutation to fasta.
+                if wtNSeq[pos] == wt:         
+                    MutNSeq=MutNSeq[1:pos] + mut + MutNSeq[pos+1:]
+                     
             fasta.write(">"+sub)
             fasta.write('\n')
-            fasta.write(wtNSeq[1:pos] + mut + wtNSeq[pos+1:])
-            fasta.write('\n')
+            fasta.write(MutNSeq)
+            fasta.write('\n')   
+
+        else:                
+            wt=sub[0]
+            mut=sub[len(sub)-1]
+            pos=int(sub[1:len(sub)-1])-29557 #29557 is ORF10 start position in sarsCov2 genome. 
+            #Check that wt allele in sub is what it should be according to wtNSeq. Write new nt seq with mutation to fasta.
+            if wtNSeq[pos] == wt:
+                fasta.write(">"+sub)
+                fasta.write('\n')
+                fasta.write(wtNSeq[1:pos] + mut + wtNSeq[pos+1:])
+                fasta.write('\n')
 fasta.close()
