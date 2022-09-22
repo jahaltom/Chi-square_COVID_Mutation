bbcc92d4d2483f85b10c6aed454908eafb73d774
Jeff Haltom <52675783+jahaltom@users.noreply.github.com>
2022-09-22 09:46:09 -0500

    Create runall.sh

---
+++ runall.sh
@@ -0,0 +1,44 @@
+python DataProcessing.py 
+python ORF10_Chi-Square_Test.py
+python ntMuts2AAMuts.py
+python DataCuration.py
+
+sed -i 's/Delta/Alpha/g' DataProcessing.py
+sed -i 's/Delta/Alpha/g' ORF10_Chi-Square_Test.py
+sed -i 's/Delta/Alpha/g' ntMuts2AAMuts.py
+sed -i 's/Delta/Alpha/g' DataCuration.py
+python DataProcessing.py 
+python ORF10_Chi-Square_Test.py
+python ntMuts2AAMuts.py
+python DataCuration.py
+
+
+sed -i 's/Alpha/Beta/g' DataProcessing.py
+sed -i 's/Alpha/Beta/g' ORF10_Chi-Square_Test.py
+sed -i 's/Alpha/Beta/g' ntMuts2AAMuts.py
+sed -i 's/Alpha/Beta/g' DataCuration.py
+python DataProcessing.py 
+python ORF10_Chi-Square_Test.py
+python ntMuts2AAMuts.py
+python DataCuration.py
+
+sed -i 's/Beta/Gamma/g' DataProcessing.py
+sed -i 's/Beta/Gamma/g' ORF10_Chi-Square_Test.py
+sed -i 's/Beta/Gamma/g' ntMuts2AAMuts.py
+sed -i 's/Beta/Gamma/g' DataCuration.py
+python DataProcessing.py 
+python ORF10_Chi-Square_Test.py
+python ntMuts2AAMuts.py
+python DataCuration.py
+
+sed -i 's/Gamma/Omicron/g' DataProcessing.py
+sed -i 's/Gamma/Omicron/g' ORF10_Chi-Square_Test.py
+sed -i 's/Gamma/Omicron/g' ntMuts2AAMuts.py
+sed -i 's/Gamma/Omicron/g' DataCuration.py
+python DataProcessing.py 
+python ORF10_Chi-Square_Test.py
+python ntMuts2AAMuts.py
+python DataCuration.py
+
+
+
